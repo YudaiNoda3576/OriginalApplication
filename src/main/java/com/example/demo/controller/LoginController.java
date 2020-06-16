17e2b5c81c361ddbd383d8a3f7ca1c936a3f097e
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-06-16 09:26:26 +0900

    ログイン画面・サインアップ画面の作成

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -1,10 +1,23 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+	@GetMapping("/login")
+	public String getLogin(Model model) {
+
+		return "register/login";
+	}
+	
+	@PostMapping("/login")
+	public String postLogin(Model model) {
+		return "redirect:/home";
+	}
 }
