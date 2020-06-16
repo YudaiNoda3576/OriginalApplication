f38e35215467cc843ff803187ba27689f4b3b57e
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-06-16 18:03:54 +0900

    新規登録画面のバリデーションを実装

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,22 +1,33 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.form.GroupOrder;
+import com.example.demo.form.SignupForm;
 
 @Controller
 @RequestMapping("/")
 public class SignupController {
 
 	@GetMapping("/signup")
-	public String  getSignUp(Model model) {
+	public String  getSignUp(@ModelAttribute SignupForm signupForm, Model model) {
 		return "register/signup.html";
 	}
 	
 	@PostMapping("/signup")
-	public String postSignup(Model model) {
+	public String postSignup(@ModelAttribute @Validated(GroupOrder.class) SignupForm signupForm, BindingResult bindingResult, Model model) {
+		if(bindingResult.hasErrors()) {
+			return getSignUp(signupForm, model);
+		}
+		
 		return "redirect:/login";
 	}
 }
