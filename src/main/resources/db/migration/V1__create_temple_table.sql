41514839f899a6b271c9afe88b9a9d1d7270f02a
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-06-23 09:51:11 +0900

    コンフリクトを解消

--- src/main/resources/db/migration/V1__create_temple_table.sql
+++ src/main/resources/db/migration/V1__create_temple_table.sql
@@ -1,25 +1,14 @@
 CREATE TABLE `temple`
 (
-<<<<<<< HEAD
     `id`        int    NOT NULL AUTO_INCREMENT,
-=======
-    `id`        int          NOT NULL AUTO_INCREMENT,
->>>>>>> master
     `temple_id`  varchar(255) NOT NULL,
     `password`  varchar(255) NOT NULL,
     `temple_name` varchar(255) NOT NULL,
     `denomination` varchar(255) NOT NULL,
-<<<<<<< HEAD
-    `name` varchar(255) NOT NULL,
-    `position` varchar(255) NOT NULL,
-    `postalcode` varchar(255) NOT NULL,
-    `adress` varchar(255) NOT NULL,
-=======
     `monk_name` varchar(255) NOT NULL,
     `position` varchar(255) NOT NULL,
     `postalcode` varchar(255) NOT NULL,
     `address` varchar(255) NOT NULL,
->>>>>>> master
     `email`     varchar(255) NOT NULL,
     `role`      varchar(32)  ,
     PRIMARY KEY (`id`)
