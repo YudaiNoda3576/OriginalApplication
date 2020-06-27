54d0bd6f3a40925643750d6e73547820525fd1c7
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-06-27 16:20:17 +0900

    マイグレーションファイルの作成

---
+++ src/main/resources/db/migration/V3__create_worship_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `worship`
+(
+    `worship_id` int NOT NULL AUTO_INCREMENT,
+    `worship_opts`  tinyint NOT NULL,
+    `schedule`  datetime NOT NULL,
+    `remark` text(500) NOT NULL,
+    `supporter_id` int NOT NULL,
+    `progress` int DEFAULT 1,
+    foreign key worship(supporter_id) references supporter(supporter_id),
+    PRIMARY KEY (`worship_id`)
+); 
