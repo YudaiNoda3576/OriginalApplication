3bc57cd49c53bd45728548140f6a216c4a06e731
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-08-26 08:57:23 +0900

    テスト

--- src/test/java/com/example/demo/OriginalApplicationApplicationTests.java
+++ src/test/java/com/example/demo/OriginalApplicationApplicationTests.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-public class OriginalApplicationApplicationTest {
+class OriginalApplicationApplicationTests {
+
 	@Test
 	void contextLoads() {
 	}
